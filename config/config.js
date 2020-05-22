9a2f44b9b71cb1eecb3b8fc32d41280a75c11396
zhaoyoushui <zhaoyoushui.163.com>
2020-05-22 15:09:39 +0800

    wzry init

---
+++ config/config.js
@@ -0,0 +1,14 @@
+// 服务接口地址
+const base = 'http://yii2adv.study.webstudy.cc/api'
+
+export default {
+  heroList: base + '/v1/heroes/heroinfo',
+  heroDetail: base + '/v1/heroes/heroDetail',
+  word: base + '/word',
+  wordNav: base + '/wordNav',
+  wordDetail: base + '/wordDetail',
+  skin: base + '/skin',
+  voiceNav: base + '/voiceNav',
+  voiceDetail: base + '/voiceDetail',
+  music: base + '/music'
+}
---
+++ pages/scrollview/scrollview.js
@@ -0,0 +1,101 @@
+// pages/scrollview/scrollview.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    toView: 'demo3'
+  },
+  upper(e) {
+    console.log(e)
+  },
+
+  lower(e) {
+    console.log(e)
+  },
+
+  scroll(e) {
+    console.log(e)
+  },
+
+  scrollToTop() {
+    this.setAction({
+      scrollTop: 0
+    })
+  },
+
+  tap() {
+    for (let i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1],
+          scrollTop: (i + 1) * 200
+        })
+        break
+      }
+    }
+  },
+  tapMove() {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: 'scroll-view',
+      path: 'pages/scroll-view/scroll-view'
+    }
+  }
+})
